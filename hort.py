9a3b89707f6f8aa583c1d72ba361aaf257203d65
malkhara <malkhara@heidelberg.edu>
2016-08-31 14:12:40 -0400

    Python code

---
+++ hort.py
@@ -0,0 +1,14 @@
+# Hedge's idea 
+#  Program to flip a coin
+
+import random
+
+flip = random.randint(0,1)
+
+if (flip ==0):
+    flipWord = 'heads'
+else:
+    flipWord = 'tails'
+
+print('I flipped, and got', flipWord)
+
