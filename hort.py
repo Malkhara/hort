eeca326af2ca9f55665c456148d6694ea07a8856
Malkhara <Malkhara@heidelberg.edu>
2017-03-13 20:41:45 -0400

    Update hort.py

--- hort.py
+++ hort.py
@@ -1,14 +1,27 @@
-# Hedge's idea 
+# Mohammd
 #  Program to flip a coin
 
 import random
 
-flip = random.randint(0,1)
-
-if (flip ==0):
-    flipWord = 'heads'
+numFlips = int(input("How many times do you want to flip? "))
+count = 0
+heads = 0
+tails = 0
+while count < numFlips:
+        flip = random.randint(0,1)
+        if flip == 0 :
+                flipWord = 'heads'
+                heads +=1
+        else:
+            flipWord = 'tails'
+            tails += 1
+        count += 1
+        print('I flipped, and got', flipWord)
+print('\nHeads: ', heads)
+print('\nTails: ', tails)
+if heads > tails:
+        print('\nHeads win')
+elif tails > heads:
+        print('\nTails win')
 else:
-    flipWord = 'tails'
-
-print('I flipped, and got', flipWord)
-
+        print('\nTie')
